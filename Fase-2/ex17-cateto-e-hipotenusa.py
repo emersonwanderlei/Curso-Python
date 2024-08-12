18fd57f64be09812b36bd93b4028778c3fef92af
Emerson Wanderlei <emerson.wanderlei03@gmail.com>
2024-08-12 14:11:53 -0300

    Update ex17-cateto-e-hipotenusa.py

--- Fase-2/ex17-cateto-e-hipotenusa.py
+++ Fase-2/ex17-cateto-e-hipotenusa.py
@@ -1,5 +1,7 @@
-import math
+#import math
+from math import hypot
 o = float(input("Qual o comprimento do cateto oposto? "))
 a = float(input('Qual o compimento do cateto adjacente? '))
-h = math.hypot(o, a) #Formula para calcular a Hipotenusa "Teorema de Pitagoras"
+#h = math.hypot(o, a) #Formula para calcular a Hipotenusa "Teorema de Pitagoras"
+h = hypot(o,a)
 print('Tendo em vista que o cateto oposto é {} e o cateto adjacente é {}. O comprimento da hipotenusa é {:.2f}'.format(o, a, h))
