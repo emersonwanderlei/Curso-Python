1d64c2d502d850529fa64528a0fe0cebe2fd22e8
Emerson Wanderlei <emerson.wanderlei03@gmail.com>
2024-08-13 20:16:07 -0300

    Update ex18-seno-cos-tan.py
    Alteração nas variaveis do sen, cos, tan

--- Fase-2/ex18-seno-cos-tan.py
+++ Fase-2/ex18-seno-cos-tan.py
@@ -1,8 +1,8 @@
 import math
 n = float(input('Qual ângulo deseja calcular? '))
-sen = math.sin(n)
-cos = math.cos(n)
-tan = math.atan(n)
+sen = math.sin(math.radians(n))
+cos = math.cos(math.radians(n))
+tan = math.atan(math.radians(n))
 print("O seno de {} é {:.2f}".format(n, sen))
 print('O cosseno de {} é {:.2f}'.format(n, cos))
 print('A tangente de {} é {:.2f}'.format(n, tan))
