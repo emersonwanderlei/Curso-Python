d5156601c5b3de3b4fe0dd7e61f6a83fb11154e4
Emerson Wanderlei <emerson.wanderlei03@gmail.com>
2024-08-14 15:49:56 -0300

    Create new file

--- Fase-2/ex20-sortear-ordem-apresentacao.py
+++ Fase-2/ex20-sortear-ordem-apresentacao.py
@@ -1,8 +1,10 @@
-from random import choice
+from random import shuffle
 print('Ordem para apresentação do trabalho, digite quatro nomes')
-n1 = input('Primeiro nome')
-n2 = input('Segundo nome')
-n3 = input('Terceiro nome')
-n4 = input('Quarto nome')
+n1 = input('Primeiro nome: ')
+n2 = input('Segundo nome: ')
+n3 = input('Terceiro nome: ')
+n4 = input('Quarto nome: ')
 lista = [n1, n2, n3, n4]
-ordem = choice(lista)
+shuffle(lista) #Embaralhar sequencia da lista
+print('A ordem de apresentação é:')
+print(lista)
