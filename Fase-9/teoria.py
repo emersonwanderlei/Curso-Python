d7127b1128375e1e5611308d1d035fa633623265
Emerson Wanderlei <emerson.wanderlei03@gmail.com>
2024-08-19 20:43:07 -0300

    Update teoria.py

--- Fase-9/teoria.py
+++ Fase-9/teoria.py
@@ -7,4 +7,4 @@ com replace(), upper(), lower(), capitalize(), title(), strip(), junção com jo
 frase = ('Curso em Video Python')
 print(frase.count('o')) # Contar quantidade de letras
 print(frase.upper()) # Deixar em caracteris maiusculos
-print(frase.lower() # Deixar os caracteris em minisculo)
+print(frase.lower()) # Deixar os caracteris em minisculo)
