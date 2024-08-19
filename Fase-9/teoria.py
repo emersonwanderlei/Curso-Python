f6cd45421470809a4083c60914cca836b285dce5
Emerson Wanderlei <emerson.wanderlei03@gmail.com>
2024-08-19 20:06:39 -0300

    Create teoria.py

---
+++ Fase-9/teoria.py
@@ -0,0 +1,10 @@
+'''
+Nessa aula, vamos aprender operações com String no Python.
+As principais operações que vamos aprender são o Fatiamento de String, Análise com len(), count(), find(), transformações
+com replace(), upper(), lower(), capitalize(), title(), strip(), junção com join().
+'''
+
+frase = ('Curso em Video Python')
+print(frase.count('o')) # Contar quantidade de letras
+print(frase.upper()) # Deixar em caracteris maiusculos
+print(frase.lower() # Deixar os caracteris em minisculo)
