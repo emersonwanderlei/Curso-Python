77cd3454124acbe4fdf3449ce97d123639ecfc24
Emerson Wanderlei <emerson.wanderlei03@gmail.com>
2024-08-20 12:45:11 -0300

    update e rename

--- Fase-9/contar-letras.py
+++
@@ -1,3 +0,0 @@
-nome = (input('Favor, digite seu nome: '))
-m = len.nome()
-print('Tem {} caracteris'.format(nome))
---
+++ Fase-9/ex022-analisar-texto.py
@@ -0,0 +1,5 @@
+nome = (input('Favor, digite seu nome para fazermos uma analize: '))
+ma = nome.upper()
+mi = nome.lower()
+
+print('Tem {} caracteris'.format(nome))
