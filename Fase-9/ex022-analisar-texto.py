f8c17d2568f8e574894e2586cf5cec281c546a99
Emerson Wanderlei <emerson.wanderlei03@gmail.com>
2024-08-20 18:49:51 -0300

    Update ex022-analisar-texto.py

--- Fase-9/ex022-analisar-texto.py
+++ Fase-9/ex022-analisar-texto.py
@@ -1,6 +1,7 @@
 nome = (input('Favor, digite seu nome para fazermos uma análise: '))
 ma = nome.upper()
 mi = nome.lower()
-
+separar = len(nome) - nome.count(nome)
 print('Seu nome em maiusculo fica {}'.format(ma))
 print('Seu nome em minisculo fica {}'.format(mi))
+print('A quantidade de letras é {}'.format(separa))
