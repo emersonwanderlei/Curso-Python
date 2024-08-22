10bceacf9f05746997dbd8d639cea0fb56abd23b
Emerson Wanderlei <emerson.wanderlei03@gmail.com>
2024-08-22 15:53:01 -0300

    Update ex022-analisar-texto.py

--- Fase-9/ex022-analisar-texto.py
+++ Fase-9/ex022-analisar-texto.py
@@ -1,4 +1,4 @@
-nome = (input('Favor, digite seu nome para fazermos uma análise: ')).strip() #strip == retirar espaços antes ou depois da palavra
+'''nome = (input('Favor, digite seu nome para fazermos uma análise: ')).strip() #strip == retirar espaços antes ou depois da palavra
 ma = nome.upper()
 mi = nome.lower()
 separar = len(nome) - nome.count(' ') #remover espaço entre uma palavra
@@ -7,5 +7,11 @@ print('Seu nome em minisculo fica {}'.format(mi))
 print('Ao todo o nome {} tem {} letras'.format(nome, separar))
 primeiroNome = nome.find(' ')
 print('Seu primeiro nome tem {} letras'.format(primeiroNome))
-
+'''
 #REFAZER QUANDOR VISUALIZAR NOVAMENTE
+
+nome = str(input('Digite seu nome pra fazer uma analise ?')).strip()
+print("Seu nome em maisculo é {}".format(nome.upper()))
+print('Seu nome em minusculo é {}'.format(nome.lower()))
+print('O nome {} tem {} letras'.format(nome, len(nome) - nome.count(' ')))
+print('Seu primeiro nome tem {}'.format(nome.find(' ')))
