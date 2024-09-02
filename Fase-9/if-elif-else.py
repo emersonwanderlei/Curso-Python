0f4dec7d797f544b4357096ec255622cb41c4f1b
Emerson Wanderlei <emerson.wanderlei03@gmail.com>
2024-09-02 17:20:33 -0300

    Update if-elif-else.py

--- Fase-9/if-elif-else.py
+++ Fase-9/if-elif-else.py
@@ -1,3 +1,10 @@
 #Introdução ao If |   elif    | else
 #              se | se não se | se não
-entrada = input('Você quer "entrar" ou "sair"? : ')
+entrada = input('Você quer "entrar" ou "sair"? : ').lower()
+
+if entrada == "entrar":
+    print("Bem-vindo! Você acessou o sistema.")
+elif entrada == "sair":
+    print('Você saiu do sistema')
+else:
+    print('ERRO. Você não digitou nem "entrar" e nem "sair"')
