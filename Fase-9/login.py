81e4e495be87b2f9bfe1900f31eb46a5e524f35d
Emerson Wanderlei <emerson.wanderlei03@gmail.com>
2024-09-04 20:53:49 -0300

    update

---
+++ Fase-9/login.py
@@ -0,0 +1,9 @@
+entrada = input('[E]ntrar [S]air: ')
+senha_digitada = input('Senha: ')
+
+senha_permitida = '12345'
+
+if entrada == 'E' and senha_digitada == senha_permitida:
+    print('Entrar')
+else:
+    print('Sair')
---
+++ op-de-comparacao-if-else.py
@@ -0,0 +1,9 @@
+n1 = input('Digite um valor: ')
+n2 = input('Digite outro valor: ')
+
+if n1 > n2:
+    print('{} é maior do que {}'.format(n1, n2))
+elif n1 < n2:
+    print('{} é menor do que {}'.format(n1, n2))
+else:
+    print('Não entendi!')
