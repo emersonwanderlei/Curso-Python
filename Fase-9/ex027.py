9be1f92a4a2be381eb02757a3eada24fcee90ec4
Emerson Wanderlei <emerson.wanderlei03@gmail.com>
2024-09-06 15:42:22 -0300

    Create ex027.py

---
+++ Fase-9/ex027.py
@@ -0,0 +1,6 @@
+n = str(input('Digite seu nome: '))
+nome = n.split()
+print('Seu primeiro nome é {}'.format(nome[0]))
+print('Seu ultimo nome é {}'.format(nome[len(nome)-1]))
+
+#REFAZER QUANDO VISUALIZAR NOVAMENTE
