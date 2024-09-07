8de979067fcde2772cd26b9e756195536d1a7e6c
Emerson Wanderlei <emerson.wanderlei03@gmail.com>
2024-09-07 19:32:36 -0300

    Update ex026-1nome-e-ultimo-nome.py

--- Fase-9/ex026-1nome-e-ultimo-nome.py
+++ Fase-9/ex026-1nome-e-ultimo-nome.py
@@ -1,4 +1,4 @@
 frase = str(input('Digite uma frase: ')).upper().strip()
 print('Essa frase tem {} letras "A"'.format(frase.count('A')))
 print('O primeiro "A" está na {}ª posição'.format(frase.find('A')+1))
-print('A ultima letra "A" está na {}ª posição'.format(frase.rfind('A')+1))
+print('A ultima letra "A" está na {}ª posição'.format(frase.rfind('A')))
