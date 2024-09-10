20f3ce52adf9a890b31f921f4614b447c9bc24cc
Emerson Wanderlei <emerson.wanderlei03@gmail.com>
2024-09-10 20:45:12 -0300

    Update ex-udemy2.py

--- Fase-9/ex-udemy2.py
+++ Fase-9/ex-udemy2.py
@@ -3,13 +3,13 @@ Faça um programa que peça ao usuário para digitar um número inteiro,
 informe se este número é par ou ímpar. Caso o usuário não digite um número
 inteiro, informe que não é um número inteiro.
 """
-'''n1 = int(input('Digite um número inteiro: '))
+n1 = int(input('Digite um número inteiro: '))
 
 if n1 % 2 == 0:
     print('Este é um número par')
 else:
     print('Este é um numero impar')
-'''
+
 """
 Faça um programa que pergunte a hora ao usuário e, baseando-se no horário 
 descrito, exiba a saudação apropriada. Ex. 
@@ -23,8 +23,18 @@ elif entrada >= 12 and entrada <= 17:
     print('Boa tarde!')
 else:
     print('Boa noite!')
+
 """
 Faça um programa que peça o primeiro nome do usuário. Se o nome tiver 4 letras ou 
 menos escreva "Seu nome é curto"; se tiver entre 5 e 6 letras, escreva 
 "Seu nome é normal"; maior que 6 escreva "Seu nome é muito grande". 
 """
+nome = input('Qual seu nome? ')
+contar = (len(nome))
+
+if contar <= 4:
+    print('Seu nome é curto')
+elif contar <= 6 and contar >= 5:
+    print('Seu nome é normal')
+else:
+    print('Seu nome é muito grande')
