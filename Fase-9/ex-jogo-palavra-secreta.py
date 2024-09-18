136e400d39a192535b86b3914b5a06d625eb862b
Emerson Wanderlei <emerson.wanderlei03@gmail.com>
2024-09-17 21:19:05 -0300

    Update ex-jogo-palavra-secreta.py

--- Fase-9/ex-jogo-palavra-secreta.py
+++ Fase-9/ex-jogo-palavra-secreta.py
@@ -1,8 +1,9 @@
 palavra_secreta = 'Exercito'
-palavra_digitada = ''
+palavra_digitada = input('Digite para verificar qual a palavra secreta: ')
 indice = 0
 while True:
 
     try:
         ...
     except:
+        ...
