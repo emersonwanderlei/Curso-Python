91a3adff2ed135cf007643e118b0bb8ceaa9eaea
Emerson Wanderlei <emerson.wanderlei03@gmail.com>
2024-09-27 21:38:37 -0300

    new update

--- Fase-9/anotacoes-python.py
+++ Fase-9/anotacoes-python.py
@@ -29,9 +29,13 @@ Criar, ler, alterar, apagar = lista[i] (CRUD)
 nomes = ('Maria', 'Helena', 'Luiz')
 """
 '''
-split e join com list e str
+***** split e join com list e str *****
+
 split - divide uma string (list)
 join - une uma string
+strip - Elimina todos os espaços
+rstrip - Elimina os espaços da direita
+lstrip - Elimina os espaços da esquerda
 
 frase = '   Olha só que   , coisa interessante          '
 lista_frases_cruas = frase.split(',')
@@ -44,4 +48,29 @@ for i, frase in enumerate(lista_frases_cruas):
 # print(lista_frases)
 frases_unidas = ', '.join(lista_frases)
 print(frases_unidas)
+'''
+'''
+# Desempacotamento em chamadas
+# de métodos e funções
+string = 'ABCD'
+lista = ['Maria', 'Helena', 1, 2, 3, 'Eduarda']
+tupla = 'Python', 'é', 'legal'
+salas = [
+    # 0        1
+    ['Maria', 'Helena', ],  # 0
+    # 0
+    ['Elaine', ],  # 1
+    # 0       1       2
+    ['Luiz', 'João', 'Eduarda', ],  # 2
+]
+
+# p, b, *_, ap, u = lista
+# print(p, u, ap)
+
+# print('Maria', 'Helena', 1, 2, 3, 'Eduarda')
+# print(*lista)
+# print(*string)
+# print(*tupla)
+
+print(*salas, sep='\n')
 '''
