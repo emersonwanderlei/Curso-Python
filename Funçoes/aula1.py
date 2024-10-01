85a40306c35b525cabb06fbd1aa633659d50c487
Emerson Wanderlei <emerson.wanderlei03@gmail.com>
2024-10-01 07:04:22 -0300

    Create aula1.py

---
+++ Funçoes/aula1.py
@@ -0,0 +1,13 @@
+'''
+Introdução as funções (def) em Python
+Funções são trechos de código usados para
+replicar determinada ação ao longo do seu código
+Elas podem receber valores para parâmetros (argumentos)
+e retornar um valor especìfico.
+Por padrão, funções Python retonan None (Não valor/nada)
+
+Argumentos nomeados e não nomeados em funções Python
+Argumento nomeado tem nome com sinal de igual
+Argumento não nomeado recebe apenas o argumento (valor)
+
+'''
