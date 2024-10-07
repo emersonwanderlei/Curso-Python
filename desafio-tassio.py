9db99d471007d1357150ca0a385c10e40136ad89
Emerson Wanderlei <emerson.wanderlei03@gmail.com>
2024-10-07 11:41:30 -0300

    Update desafio-tassio.py

--- desafio-tassio.py
+++ desafio-tassio.py
@@ -1,20 +1,44 @@
-senha_permitida = 'Wander7!'
-caracter_especiais = '!@#$%^&*()'
+
+
+
+def contem_caracteres_especiais(senha):
+    caracter_especiais = '!@#$%^&*()'
+
+    for letra in senha:
+        if letra in caracter_especiais:
+            return True
+
+    return False
+
+def contem_letra_maiscula(senha):
+
+    for letra_mais in senha:
+        if letra_mais.isupper():
+            return True
+    return False
+
+def contem_letra_miniscula(senha):
+
+    for letra_minis in senha:
+        if letra_minis.islower():
+            return True
+    return False
+
+def contem_numeros(senha):
+
+    for numero in senha:
+        if numero.isdigit():
+            return True
+    return False
 
 def login():
     while True:
-        senha_digitada = input('Digite sua senha: ')
-
-        if senha_digitada in senha_permitida:
-            print('Login permitido')
-        if senha_digitada != senha_permitida:
-            print('Senha não confere, digite novamente')
-            continue
-        if len(senha_digitada) == 8:
-            print('Login permitido')
+        senha = input('Digite sua senha com 8 digitos, 1 caracter especial, 1 letra maiuscula, 1 letra minuscula: ')
+        if len(senha) == 8 and contem_caracteres_especiais(senha) and contem_letra_maiscula(senha) and contem_letra_miniscula(senha) and contem_numeros(senha):
+            print('Senha Valida.')
+            break
         else:
-            print('Digite pelo menos 1 caracter especial')
-                
+            print('Senha Invalida!')    
 
-
+print('VALIDADOR DE SENHA')      
 login()
