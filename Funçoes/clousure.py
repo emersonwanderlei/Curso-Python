45f446816108ce8208e41a3fdd49ad49369ba890
Emerson Wanderlei <emerson.wanderlei03@gmail.com>
2024-10-09 10:51:09 -0300

    new

---
+++ Funçoes/clousure.py
@@ -0,0 +1,5 @@
+
+def criar_saudacao(saudacao, nome):
+    return f'{saudacao}, {nome}'
+
+print(criar_saudacao('Boa tarde', 'Emerson'))
--- Funçoes/ex01.py
+++ Funçoes/ex01.py
@@ -5,12 +5,21 @@
 # Retorne o total para uma variável e mostre o valor
 # da variável.
 
-def multiplicar(*args):
-    total = 1
-    for numeros in args:
-        total *= numeros
-    return total
+# def multiplicar(*args):
+#     total = 1
+#     for numeros in args:
+#         total *= numeros
+#     return total
 
 
-multiplicacao = multiplicar(1, 2, 3, 4, 5)
-print(multiplicacao)
+# multiplicacao = multiplicar(1, 2, 3, 4, 5)
+# print(multiplicacao)
+
+def par_impar():
+    numero = input('Digite um numero: ')
+    if int(numero) % 2 == 0:
+        return f'O numero {numero} é par'
+    return f'O numero {numero} é impar'
+    
+multiplos = par_impar()
+print(multiplos)
