3ff9e0c8d4a280226d2fa8324b586b2cf9f918fd
Emerson Wanderlei <emerson.wanderlei03@gmail.com>
2024-10-13 20:37:55 -0300

    new

--- dicionario/aula1.py
+++ dicionario/aula1.py
@@ -1,12 +1,31 @@
+# pessoa = {
+#     'nome': 'Emerson',
+#     'sobrenome': 'Wanderlei',
+#     'idade': '26',
+#     'altura': 1.66,
+#     'endereço': [
+#         {'rua': 'Candido Reis', 'numero': 601},
+#         {'rua': 'Estrada do Coelho', 'numero': 36}]
+# }
+
+# for chaves in pessoa:
+#     print(chaves, pessoa[chaves])
+
+#MANIPULÇÃO DE DICIONARIO
+#*********CRUD***********
+
+# pessoa = {} # Criar dicionario
+
+# chave = 'nome'
+
+# pessoa[chave] = 'Emerson' # Atualizar dicionario incluindo itens
+# pessoa['sobrenome'] = 'Wanderlei' # Atualizar dicionario incluindo itens
+
+# del pessoa['sobrenome'] # Apagar itens do dicionario
+# print(pessoa)
+
 pessoa = {
     'nome': 'Emerson',
     'sobrenome': 'Wanderlei',
-    'idade': '26',
-    'altura': 1.66,
-    'endereço': [
-        {'rua': 'Candido Reis', 'numero': 601},
-        {'rua': 'Estrada do Coelho', 'numero': 36}]
+    'idade': 0,
 }
-
-for chaves in pessoa:
-    print(chaves, pessoa[chaves])
