a79d1ebf5accba1c6e87c4450594802830b2c81c
Emerson Wanderlei <emerson.wanderlei03@gmail.com>
2024-10-20 15:07:47 -0300

    commit

--- dicionario/ex-sistema-de-perguntas.py
+++ dicionario/ex-sistema-de-perguntas.py
@@ -38,6 +38,26 @@ perguntas = [
         'Resposta': '5',
     },
 ]
+qtd_acertou = 0
+for pergunta in perguntas:
+    questoes = pergunta['Pergunta']
+    print(questoes)
+    print()
 
-for pergunta in list(perguntas['Pergunta']):
-    print(pergunta)
+    for i, opcoes in enumerate(pergunta['Opções']):
+        print(f'{i})', opcoes)
+    resposta = input('Escolha qual opção: ')
+    
+    resposta_int = None
+    if resposta.isdigit():
+        resposta_int = int(resposta)
+
+    if resposta_int >= 0 and resposta_int <= len(opcoes):
+        acertou = False
+        if resposta_int == pergunta['Resposta']:
+            acertou = True
+        if acertou:
+            print('Você ACERTOU!')
+        else:
+            print('Você ERROU')
+    break
---
+++ dicionario/lista-de-exercicios.py
@@ -0,0 +1,12 @@
+# Crie um dicionário que represente informações sobre uma pessoa,
+# como nome, idade, cidade natal e profissão.
+
+pessoa = {
+    'nome': 'Emerson',
+    'idade': 26,
+    'cidade natal' : 'São Gonçalo-RJ',
+    'profissão': 'Militar',
+}
+
+for lista in pessoa:
+    print(lista, pessoa[lista])
