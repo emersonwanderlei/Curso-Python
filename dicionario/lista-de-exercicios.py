65c2a566f8e0e0a57a76a3ef595435d21c60b93e
Emerson Wanderlei <emerson.wanderlei03@gmail.com>
2024-10-23 20:45:31 -0300

    Update lista-de-exercicios.py

--- dicionario/lista-de-exercicios.py
+++ dicionario/lista-de-exercicios.py
@@ -1,4 +1,4 @@
-# Crie um dicionário que represente informações sobre uma pessoa,
+# 1- Crie um dicionário que represente informações sobre uma pessoa,
 # como nome, idade, cidade natal e profissão.
 
 pessoa = {
@@ -7,6 +7,12 @@ pessoa = {
     'cidade natal' : 'São Gonçalo-RJ',
     'profissão': 'Militar',
 }
+
+pessoa['nome'] = 'Matheus'
+pessoa.update({
+    'email': 'emerson.wanderlei03@gmail.com',
+    'cor': 'Negão',
+})
 
 for lista in pessoa:
     print(lista, pessoa[lista])
